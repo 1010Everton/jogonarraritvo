f45d126c87ff90c450553ecdfebf472b7caedaf3
Everton Florencio <evertonflorenciodasilva@outlook.com>
2024-02-12 11:52:21 -0300

    inicio do projeto do jogo narrativo

---
+++ painel_principal_do_jogo.java
@@ -0,0 +1,8 @@
+public class painel_principal_do_jogo {
+    String iniciar = "novo jogo";
+    String carregar = "carregar o jgoo";
+    String multijogador =  "jogar com duas pessoas";
+    String configuracoes =  "congiguranções";
+
+
+}
