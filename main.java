e97f0703dc634babd310386d48b844c90e039170
Everton Florencio <evertonflorenciodasilva@outlook.com>
2024-02-18 13:52:38 -0300

    a intro do jogo

--- main.java
+++ main.java
@@ -1,2 +1,10 @@
 public class main {
+    public static void main(String[] args) {
+        painel_principal_do_jogo face = new painel_principal_do_jogo();
+        System.out.println(face.getIniciar());
+        System.out.println(face.getMultijogador());
+        System.out.println(face.getCarregar());
+        System.out.println(face.getConfiguracoes());
+        System.out.println(face.getSair());
+    }
 }
--- painel_principal_do_jogo.java
+++ painel_principal_do_jogo.java
@@ -1,8 +1,40 @@
-public class painel_principal_do_jogo {
-    String iniciar = "novo jogo";
-    String carregar = "carregar o jgoo";
-    String multijogador =  "jogar com duas pessoas";
-    String configuracoes =  "congiguranções";
 
+/*
+    Nome do Projeto = Herdeiros da alura
+    autor = Everton Florencio da silva
+
+ */
+
+public class painel_principal_do_jogo {
+    private String iniciar = "novo jogo";
+    private String carregar = "carregar o jgoo";
+    private String multijogador = "jogar com duas pessoas";
+    private String configuracoes = "congiguranções";
+    private String sair = "sair";
+
+    public String getMultijogador() {
+        return multijogador;
+    }
+
+    public String getIniciar() {
+        return iniciar;
+    }
+
+    public String getCarregar() {
+        return carregar;
+    }
+    public String getConfiguracoes() {
+        return configuracoes;
+    }
+
+    public String getSair() {
+        return sair;
+    }
 
 }
+
+
+
+
+
+
